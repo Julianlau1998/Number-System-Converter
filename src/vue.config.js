9daa4243d032b7c7364e282fb7d31ff364f3b482
Julian Lau <julian.lau@aol.com>
2022-01-29 13:23:56 +0100

    theme color

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   pwa: {
     name: 'Number Systems Converter',
-    themeColor: '#bfbfbf',
-    msTileColor: '#bfbfbf',
+    themeColor: '#2a343c',
+    msTileColor: '#1a1c1e',
+    navigationColor: "#1a1c1e",
+    navigationColorDark: "#1a1c1e",
+    navigationDividerColor: "#1a1c1e",
+    navigationDividerColorDark: "#1a1c1e",
+    backgroundColor: '#1a1c1e',
     mobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'default',
     start_url: '.',
